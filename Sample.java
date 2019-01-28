3fb54a7c5460b8c42763f396ccbc86c3566c4004
truengine <fosterdavidryan@gmail.com>
2019-01-28 15:04:46 +0000

    added new files and changed location of return 0 command

--- Sample.java
+++ Sample.java
@@ -5,11 +5,13 @@ public class Sample {
 		String bitters = "so sour!";
 
 		goNow(iters, bitters);
+
+		return 0;
 	}
 	public String goNow(int a, String bean){
 		for(int i = 0; i < a; i++){
 			System.out.println("This is " + bean + \n);
 		}
-		return 0;
+
 	}
 }
